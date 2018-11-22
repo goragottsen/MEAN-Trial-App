5b185efc738b9e1072ef4c0c9c2f183e37f5293a
goragottsen <goragottsen@gmail.com>
2018-11-22 17:51:22 -0500

    Fetch posts from derver

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    { id: 'dsjhdjsgj328472847',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    { id: 'djfhkegr7465i3g',
+      title: 'Second server-side post',
+      content: 'This is coming from the server!'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
