5b4ba4dfb1d796447fcc4fe0ca02b0f06ff2f947
goragottsen <goragottsen@gmail.com>
2018-11-22 20:00:36 -0500

    Fixed CORS

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', "*"); // no matter which domain used, it's allowed to access the resources
+  res.setHeader('Access-Control-Allow-Headers',
+  "Origin, X-Requested-With, Content-Type, Accept"); // meaning incoming requests MAY have these headers
+  res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE, OPTIONS"); // which http verbs may be used to send requests (options is sent implicitly, has to be stated)
+  next();
+});
+
 app.use('/api/posts', (req, res, next) => {
   const posts = [
     { id: 'dsjhdjsgj328472847',
